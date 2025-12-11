062583851a52d1b667cfd6e864b48913e3dbf7a7
HARI VARDHAN REDDY KUMMETHA <harivardhan888@gmail.com>
2025-12-11 16:14:04 +0530

    Initial commit with two streamlit apps

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "True"
+os.environ["LANGCHAIN_PROJECT"]="Q&A Chatbot with openai"
+
+## Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question{question}")
+    ]
+)
+
+def generate_response(question, api_key, llm, temperature, max_tokens):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model=llm)
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+    answer = chain.invoke({'question':question})
+    return answer
+
+## streamlit
+st.title("Q&A chatbot with openai")
+st.sidebar.title("settings")
+api_key=st.sidebar.text_input("Enter your Open ai api key: ", type="password")
+llm=st.sidebar.selectbox("Select an Open ai Model", ["gpt-4o", "gpt-4-turbo", "gpt-4"])
+temperature=st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
+max_tokens=st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+
+st.write("Go ahead and ask any question")
+user_input = st.text_input("You:")
+
+if user_input:
+    response = generate_response(user_input, api_key, llm, temperature, max_tokens)
+    st.write(response)
+else:
+    st.write("Please provide the query")
